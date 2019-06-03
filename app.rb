9a1cfd5a1052b8950102adff11d7f05f4c53c963
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-03 01:24:45 -0400

    created get methods in App class using sinatra routes

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Daniela"
+  end
+
+  get '/hometown' do
+    "My hometown is Ecuador"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bailar Contingo"
+  end
+
 end
