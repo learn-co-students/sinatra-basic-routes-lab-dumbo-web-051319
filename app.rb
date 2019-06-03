0e585f649e0e8e1ce2ce24b6fa5fe32703461d03
othaderek <otha.hernandez@gmail.com>
2019-06-03 16:36:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Otha Hernandez"
+  end
+
+  get '/hometown' do
+    "My hometown is Los Angeles"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Vitamin C by CAN"
+  end
 end
